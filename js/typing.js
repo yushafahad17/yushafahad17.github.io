6c7ad9413a20bbeb8f7e1ca915975aea4f105c82
yushafahad17 <fhanywibu123@gmail.com>
2023-03-14 01:35:59 +0700

    FIRST

---
+++ js/typing.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hai, Saya Fahad Abdurrahman</span> "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
